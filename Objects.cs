d982cad4dd8da52e8d7a20b152f929c8b66e006a
kungfux <kungfux@13c40c9c-29d6-4030-9b1a-ae434302ca30>
2011-06-26 15:30:32 +0000

    fixes #17
    git-svn-id: http://itworks.sytes.net/svn/quotepad/trunk@2 13c40c9c-29d6-4030-9b1a-ae434302ca30

---
+++ Objects.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace QuotePad
+{
+    public static class Objects
+    {
+        public class Theme
+        {
+            public int ID;
+            public string Name;
+        }
+
+        public class Author
+        {
+            public int ID;
+            public string FIO;
+            public string About;
+            public Image Photo;
+        }
+
+        public class Quote
+        {
+            public int ID;
+            public Author QuoteAuthor;
+            public Theme QuoteTheme;
+            public string RTF;
+            public bool IsFavorite;
+            public DateTime WhenCreated;
+        }
+    }
+}
